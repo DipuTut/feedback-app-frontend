14322ab752118971969798359dd8f44fc511629d
DipuTut <mddipu@gmail.com>
2024-10-07 22:32:10 +0200

    Adding Jenkinsfile.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,9 +1,12 @@
 docker build \
-    -t feedback-app-frontend:v1.0 \
+    -t feedback-app-frontend:v3.0 \
     -t feedback-app-frontend:latest \
-    -t asadulhaque90/feedback-app-frontend:v1.0 \
+    -t asadulhaque90/feedback-app-frontend:v3.0 \
     -t asadulhaque90/feedback-app-frontend:latest .
 
     # Push the images to Docker Hub
-    docker push asadulhaque90/feedback-app-frontend:v1.0
+    docker push asadulhaque90/feedback-app-frontend:v3.0
     docker push asadulhaque90/feedback-app-frontend:latest
+
+# Start the app with Docker Compose
+docker-compose up --build
